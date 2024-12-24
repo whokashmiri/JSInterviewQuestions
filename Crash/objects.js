f0e08c0ccb2e1f3b3e0b313a315d28ecac0aaafc
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-24 21:24:01 +0530

    Color Generator

--- Crash/objects.js
+++ Crash/objects.js
@@ -8,3 +8,40 @@ console.log(person.name); // John
 delete person.age;
 console.log(person);
 
+
+db.createCollection("students",
+    {validator:
+        {$jsonSchema:
+            {bsonType:"object",
+                required:["name","age","job"],
+                properties:
+                {name:
+                    {
+                        bsonType:"string", 
+                        description:"Name Should be A String ",
+                         maxLength:30
+                        },
+                        age:{
+                            bsonType:"int",
+                            description:"Age Should a Number",
+                            minimum:12,
+                            maximum:50
+                        },
+                        jod:
+                        {
+                            bsonType:"date",
+                            description:"Should be Date"
+                        },
+                        course:{
+                        bsonType:"array",
+                        items:{
+                        bsonType:["string"],
+                        description:"string"
+                    }
+                }
+                }
+            }
+        }
+    }
+)
+
