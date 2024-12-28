804c7e9bed7a023c05b131c20e62429b882fabb5
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-28 21:57:37 +0530

    Proto

---
+++ proto.js
@@ -0,0 +1,22 @@
+console.log("======Prototype======");
+function mul(num){
+return num*4
+}
+console.log(mul(3));
+
+console.log(mul.power = 2);
+console.log(mul.prototype);
+let str = "hello"
+let str1 = "world  "
+let str2 =''
+for (let index = 0; index < str.length; index++) {
+    for (let index = 0; index < str1.length; index++) {
+    console.log(str2.concat(str[index],str1[index]));
+    
+        // console.log ("from",str[index],str1[index]);
+        
+    }
+ 
+   
+    
+}
