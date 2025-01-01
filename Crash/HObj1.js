de61de6f53ffd8cec34e875351391da1a4972511
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-01 08:56:34 +0530

    Objects Continue

--- Crash/HObj1.js
+++ Crash/HObj1.js
@@ -24,3 +24,53 @@ console.log(Juser);
 Juser.fname();
 Juser.another.myFunc();
 
+
+//Dynamic Adding
+const pro = "fname"
+const nam = "Aaqib Mir"
+const OUser = {
+    [pro]:nam
+}
+console.log(OUser);
+
+
+const prac={
+    a:"A",
+    b:"B",
+    c:"B",
+    d:"D",
+    e: function(){
+console.log(this.a);
+    },
+    f:()=>{
+        console.log(this.a);
+        
+    }
+}
+prac.e()
+prac.f()
+for(key in prac){
+    
+    console.log(key , prac[key]);
+   
+    
+}
+
+
+let person = {name:"Aaqib"}
+const  mem = [person]
+person= null
+console.log(mem);
+
+
+const val ={num:10}
+const mul = (x ={...val})=>{
+    console.log(x.num *= 2);
+    
+}
+mul()
+mul(val) // after passing the value it will modify the val to 20. 
+mul()// then after calling the function again the total will 20*2 =40
+
+
+
