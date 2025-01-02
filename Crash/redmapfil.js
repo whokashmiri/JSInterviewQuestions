da5264f3af44ee727cbe5616d1b7e905f630fe26
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-02 14:42:25 +0530

    Notes updated

--- Crash/redmapfil.js
+++ Crash/redmapfil.js
@@ -16,7 +16,7 @@ console.log(item);
 
 
 let numMap=[1,2,3,4,5,6,7]
-// The map() method is an ES5 feature that creates a new array by applying a function to each element of the original array. 
+//  
 let newMapArr = numMap.map((num)=>num+3);
 console.log(newMapArr);
 
@@ -25,4 +25,4 @@ console.log(newMapArr);
 
 
 
-//The reduce method in JavaScript does not return a new array. Instead, it executes a reducer function on each element of the array, resulting in a single output value
+//
