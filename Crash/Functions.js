a9cf01eb709bd7d023731fad9ca597ba717ecd2a
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-16 09:15:39 +0530

    functions

--- Crash/Functions.js
+++ Crash/Functions.js
@@ -1,10 +1,33 @@
-function func(){
-    let name = "Mozi";
-    function dName(){
-        console.log(name);
+// function func(){
+//     let name = "Mozi";
+//     function dName(){
+//         console.log(name);
         
-    }
-    return dName;
+//     }
+//     return dName;
+// }
+// let myFunc = func()
+// myFunc()
+
+function add(n1 , n2){
+    console.log(n1 + n2);
+    console.warn(n1 - n2);
+    let result = n1 * n2;
+    return result ;
+    
 }
-let myFunc = func()
-myFunc()
+// add(32,43)
+// console.log(add(4,4));
+
+// console.log(add(4,2));
+function cart(...num){
+    let num1= 0
+    num.map((item)=>{
+  return num1 += item
+   
+
+    })
+    return num1
+}
+
+ console.log(cart(1,2,3,4));
