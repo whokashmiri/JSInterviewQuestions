a5ad1cd74ad50c5cd5987b9f478eddaee1aba53f
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-02-26 14:59:13 +0530

    SnapSec Interview

---
+++ Snap/01Arrays.js
@@ -0,0 +1,13 @@
+const myArr = [true,0,1,2,3,4,5]
+console.log(myArr);
+const myHeros = ["A" , "B"]
+console.log(myArr);
+
+const add = myArr.concat(myHeros)
+console.log(add);
+console.log(myArr);
+console.log(myHeros);
+
+
+
+
